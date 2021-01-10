ae4ab55adcf43e8b3447b1b0f053b4b85616dc83
Ricardo Yanomane <62074727+yanomane@users.noreply.github.com>
2021-01-10 20:00:21 -0300

    Add files via upload

---
+++ mysite/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'telefone')
+
+admin.site.register(Contato, ContatoAdmin)
+
